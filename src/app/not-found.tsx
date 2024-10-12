67094c124b6706ec178aa33c3f469fa161575427
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-10-12 21:30:58 +0600

    /404 page added

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,24 @@
-import Link from 'next/link'
+import { Result, Button } from 'antd';
+import Link from 'next/link';
+import React from 'react';
+
+const NotFoundPage: React.FC = () => {
  
-export default function NotFound() {
+
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Link href="/">
+            <Button type="primary">Back Home</Button>
+          </Link>
+        }
+      />
     </div>
-  )
-}
+  );
+};
+
+export default NotFoundPage;
