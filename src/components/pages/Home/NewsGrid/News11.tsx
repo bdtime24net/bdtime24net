07926896cc4a3896d83d719f3fd79581189ce94f
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-12-15 21:59:26 +0600

    Fake News Data json bangla

--- src/components/pages/Home/NewsGrid/News11.tsx
+++ src/components/pages/Home/NewsGrid/News11.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import newsData from '@/data/homeNews.json'; // Adjust the path as necessary
 
 const News11: React.FC = () => {
     return (
         <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16 relative">
             <div className="grid grid-cols-1 sm:grid-cols-12 gap-10">
-
                 {/* Main Article */}
                 <div className="sm:col-span-6 lg:col-span-5">
                     <a href="#">
                         <div
                             className="h-56 bg-cover text-center overflow-hidden"
                             style={{
-                                backgroundImage: "url('https://tailwindcss.com/img/card-left.jpg')",
+                                backgroundImage: `url('${newsData.news[0].imageUrl}')`,
                             }}
-                            title="Woman holding a mug"
+                            title={newsData.news[0].title}
                         ></div>
                     </a>
                     <div className="mt-3 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
@@ -22,16 +22,16 @@ const News11: React.FC = () => {
                                 href="#"
                                 className="text-xs text-indigo-600 uppercase font-medium mb-3 flex items-center hover:text-gray-900 transition duration-500 ease-in-out"
                             >
-                                Fashion
+                                {newsData.news[0].category}
                             </a>
                             <a
                                 href="#"
                                 className="text-gray-900 font-bold text-lg mb-2 hover:text-indigo-600 transition duration-500 ease-in-out"
                             >
-                                The perfect summer sweater that you can wear!
+                                {newsData.news[0].title}
                             </a>
                             <p className="text-gray-700 text-xs mt-2">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                                {newsData.news[0].description}
                             </p>
                         </div>
                     </div>
@@ -39,37 +39,16 @@ const News11: React.FC = () => {
 
                 {/* Secondary Articles */}
                 <div className="sm:col-span-6 lg:col-span-4">
-                    {[
-                        {
-                            date: "Aug 18",
-                            title: "Cristiano Ronaldo of Juventus FC looks dejected during the...",
-                            image: "https://media.gettyimages.com/photos/cristiano-ronaldo-of-juventus-fc-looks-dejected-during-the-uefa-of-picture-id1227967060?k=6&m=1227967060&s=612x612&w=0&h=cMSMlRyI6YAzcE_C2KgHGRLeVojHYoUhIvhwPBYv8f4=",
-                        },
-                        {
-                            date: "Jan 18",
-                            title: "Barcelona v Bayern Munich",
-                            image: "https://media.gettyimages.com/photos/lionel-messi-and-marcandre-ter-stegen-of-fc-barcelona-waits-in-the-picture-id1266763488?k=6&m=1266763488&s=612x612&w=0&h=8vxz9HfQVfrff5N7d1lBVxtLamRQGK3J3lyHkUuuIiw=",
-                        },
-                        {
-                            date: "Aug 18",
-                            title: "Cristiano Ronaldo of Juventus FC looks dejected during the...",
-                            image: "https://media.gettyimages.com/photos/cristiano-ronaldo-of-juventus-fc-looks-dejected-during-the-uefa-of-picture-id1227967060?k=6&m=1227967060&s=612x612&w=0&h=cMSMlRyI6YAzcE_C2KgHGRLeVojHYoUhIvhwPBYv8f4=",
-                        },
-                        {
-                            date: "July 23",
-                            title: "Barcelona v Bayern Munich - UEFA Champions League",
-                            image: "https://media.gettyimages.com/photos/lionel-messi-of-fc-barcelona-looks-dejected-following-his-teams-in-picture-id1266341828?k=6&m=1266341828&s=612x612&w=0&h=FZi-bSrIlOEE01780h79GsbBYPqZo2l3aaCxoktWADY=",
-                        },
-                    ].map((article, index) => (
+                    {newsData.news.slice(1).map((article, index) => (
                         <div className="flex items-start mb-3 pb-3" key={index}>
                             <a href="#" className="inline-block mr-3">
                                 <div
                                     className="w-20 h-20 bg-cover bg-center"
-                                    style={{ backgroundImage: `url('${article.image}')` }}
+                                    style={{ backgroundImage: `url('${article.imageUrl}')` }}
                                 ></div>
                             </a>
                             <div className="text-sm">
-                                <p className="text-gray-600 text-xs">{article.date}</p>
+                                <p className="text-gray-600 text-xs">{article.location}</p>
                                 <a
                                     href="#"
                                     className="text-gray-900 font-medium hover:text-indigo-600 leading-none"
@@ -87,9 +66,9 @@ const News11: React.FC = () => {
                         <div
                             className="h-56 bg-cover text-center overflow-hidden"
                             style={{
-                                backgroundImage: "url('https://tailwindcss.com/img/card-left.jpg')",
+                                backgroundImage: `url('${newsData.news[0].imageUrl}')`,
                             }}
-                            title="Woman holding a mug"
+                            title={newsData.news[0].title}
                         ></div>
                     </a>
                     <div className="mt-3 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
@@ -98,21 +77,20 @@ const News11: React.FC = () => {
                                 href="#"
                                 className="text-xs text-indigo-600 uppercase font-medium flex items-center hover:text-gray-900 transition duration-500 ease-in-out"
                             >
-                                Fashion
+                                {newsData.news[0].category}
                             </a>
                             <a
                                 href="#"
                                 className="text-gray-900 font-bold text-lg mb-2 hover:text-indigo-600 transition duration-500 ease-in-out"
                             >
-                                The perfect summer sweater that you can wear!
+                                {newsData.news[0].title}
                             </a>
                             <p className="text-gray-700 text-xs mt-2">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                {newsData.news[0].content}
                             </p>
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
     );
