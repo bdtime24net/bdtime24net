c44ca9ef5573c28c166c34869eb0038e4c2ae516
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-02-09 13:19:41 +0600

    build-1-1-1-1.1

--- src/app/(admin)/dashboard/news/update/[id]/page.tsx
+++ src/app/(admin)/dashboard/news/update/[id]/page.tsx
@@ -1,15 +1,26 @@
-// /src/app/update/[id].tsx
-import React from 'react';
+// /src/app/dashboard/news/update/[id].tsx
+
 import UpdateNewsForm from '@/components/molecules/v2/UpdateNewsForm';
+
+export async function generateStaticParams() {
+  // Fetch all the articles to generate static paths
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/articles`);
+  const articles = await response.json();
+
+  // Generate the params for each article, assuming `articles` is an array of objects with `id` as a string
+  return articles.map((article: { id: string }) => ({
+    id: article.id, // This is the dynamic parameter `id`
+  }));
+}
 
 interface UpdateArticlePageProps {
   params: {
-    id: string; // Define the type of the id parameter
+    id: string; // Dynamic parameter for article ID
   };
 }
 
 const UpdateArticlePage: React.FC<UpdateArticlePageProps> = ({ params }) => {
-  const { id } = params; // Get the article ID from the URL parameters
+  const { id } = params; // Get the article ID from the URL params
 
   return (
     <div className="container mx-auto p-4">
